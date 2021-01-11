1c70ebd2e38cc7645e6fcc74b459956c459100a7
luu-luongthanh-mox <luu.luong-thanh@moxbank.com>
2021-01-11 19:46:15 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
 const nextTranslate = require('next-translate')
-// const withTM = require('next-transpile-modules')(['@babel/runtime']); // pass the modules you would like to see transpiled
 
-// module.exports = withTM();
 module.exports = nextTranslate()
